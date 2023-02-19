7f3b15b6b5c474a7762a36a3e2f73b85eadb37cf
Elon Aseneka <elonsantos63@gmail.com>
2023-02-20 02:39:06 +0300

    working MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
+export type CustomResponse<T> = {
+  payload: T;
+};
+
 function App() {
+  const [userStory, setUserStory] = useState<CustomResponse<string>>();
+  const [inputVal, setInputVal] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  
+  const fetchUserStory = async (search: string) => {
+    setLoading(true)
+    const res = await fetch(`http://localhost:3000/generate-user-story?search=${search}`);
+    const data = await res.json();
+    setUserStory(data);
+    setLoading(false);
+  }
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    
+    setInputVal(e.target.search.value);
+    await fetchUserStory(e.target.search.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Generate a user story for your project</h1>
       </header>
+      <main>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="">Project domain e.g ecommerce website</label>
+          <div className="field-container">
+            <input type="text" placeholder="Search..." name="search" />
+            <button type="submit">Search</button>
+          </div>
+        </form>
+
+        <div className="content">
+          {inputVal && <h2>Here is your user story on <span className="ital">{inputVal}</span></h2>}
+          {loading && <div className="lds-hourglass"></div>}
+          <p>{userStory?.payload}</p>
+        </div>
+      </main>
     </div>
   );
 }
